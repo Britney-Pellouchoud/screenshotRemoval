9f4e48d1a18cb352984b780ef0660c15601cd5ea
Britney Pellouchoud <59316202+Britney-Pellouchoud@users.noreply.github.com>
2023-03-29 14:34:58 -0700

    Initial commit

---
+++ deleteScreenshots.py
@@ -0,0 +1,19 @@
+import os, magic
+
+def find():
+	path = os.getcwd()
+
+	for root, dirs, files in os.walk(os.path.dirname(os.getcwd()), ):
+		for name in files:
+			try:
+				if "Screenshot" in name and ".ktx" in name:
+					os.remove(name)
+			except:
+				continue
+	return
+
+
+
+
+find()
+
